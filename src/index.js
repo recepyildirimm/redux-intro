18afeb67aea9f9f1f156d71572accbbca4d3e76c
recepyildirimm <ryildirimm45@gmail.com>
2024-02-16 16:13:50 +0300

    Redux Intro

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import configureStore from "./redux/reducers/configureStore"
+import {Provider} from "react-redux"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const store = configureStore();
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
 
---
+++ src/redux/reducers/configureStore.js
@@ -0,0 +1,8 @@
+import {createStore} from "redux"
+
+import reducers from "./index"
+
+export default function configureStore(){
+
+    return createStore(reducers);
+}
